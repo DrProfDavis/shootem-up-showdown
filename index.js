1abbc6af2665f469f878ac0b6e8f9264a50ea6ef
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-08-17 21:14:27 -0500

    Sprite class set up with position

--- index.js
+++ index.js
@@ -12,7 +12,19 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-// calls our canvas context to create a rectangle
+// calls our canvas context to create a rectangle background for our game
 // fillRect takes four arguments (x-position, y-position, width, height)
-// TODO No idea how to do hexagon yet so defaulted to this - Shawn
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+// in order to create an object, we create a class that acts as the blueprint of the object before we create the object itself
+class Sprite {
+    // when you create a class, you need a constructor method, which is basically a function within the class and will be called every time we create a new object using the Sprite class
+    constructor(position) {
+        // when you create a property within a class and a constructor you need to make sure it's prefaced with 'this' so each new object has the same properties
+        // our sprites will need positions independent from one another. So each time instantiate a sprite, 'this.position' is going to be assigned the position of the individual sprite we created 
+        this.position = position
+    }
+}
+
+// creates our playerOne using the Sprite class
+const playerOne = new Sprite()
