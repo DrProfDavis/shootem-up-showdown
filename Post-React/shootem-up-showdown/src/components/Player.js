d96772743f8e2a12e697e79bd2b240dfdfe1c1d1
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-09-05 03:56:30 -0500

    Player 2 Optimized!

--- Post-React/shootem-up-showdown/src/components/Player.js
+++ Post-React/shootem-up-showdown/src/components/Player.js
@@ -1,21 +1,17 @@
 import React from "react";
-import playerImage from "../images/cowboy.png"; 
+import playerImage1 from "../images/cowboy.png"; 
 import playerImage2 from "../images/cowboy2.png"; 
 
-const Player1 = () => {
+const Player = ({ playerNumber }) => {
+
+  const playerImage = playerNumber === 1 ? playerImage1 :
+                      playerNumber === 2 ? playerImage2 :
+                      null;
+
+                      
   return (
-  
       <image className ="player" href={playerImage} width="10" height="10" />
-
   );
 };
 
-const Player2 = () => {
-  return (
-  
-      <image className ="player" href={playerImage2} width="10" height="10" />
-
-  );
-};
-
-export {Player1, Player2};
+export {Player};
