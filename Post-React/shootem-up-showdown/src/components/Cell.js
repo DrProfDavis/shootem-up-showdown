48a201efd5f0de008f48644fe64fc48f577743f2
Rockojoe2 <lee.d.joey@gmail.com>
2023-09-04 22:46:05 -0500

    Got random player spawn to work

--- Post-React/shootem-up-showdown/src/components/Cell.js
+++ Post-React/shootem-up-showdown/src/components/Cell.js
@@ -9,16 +9,40 @@ let handleClick = (i) => {
     console.log(i)
 }
 
-const Cell = ({ q, r, i }) => {
 
-    const isPlayerCell = q === 5 && r === -1; // Change these coordinates as needed
+const places = [
+    {i: 0},
+    {i: 1},
+    {i: 2},
+    {i: 17},
+    {i: 18},
+    {i: 19},
+    {i: 35},
+];
+
+const getRandomPlace = () => {
+    const randomIndex = Math.floor(Math.random() * places.length);
+    return places[randomIndex];
+};
+
+const randomPlace = getRandomPlace();
+
+
+const Cell = ({ q, r, i}) => {
+
+    
+    // const isPlayerCell = q === 5 && r === -1; // Change these coordinates as needed
+
+    const isPlayerCell = places.some(place => place.i === i);
 
     return (
         <Hexagon onClick={()=>handleClick(i)} key={i} q={q} r={r}>
-            {<Text>
+            
+            {/* {<Text>
                 {i} {q} {r}
-            </Text>}
-            {isPlayerCell ? <Player /> : null}
+            </Text>} */}
+            {/* {isPlayerCell ? <Player /> : null} */}
+            {randomPlace.i === i ? <Player /> : null}
         </Hexagon>
     )
 }
