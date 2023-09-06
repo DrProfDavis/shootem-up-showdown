a383e1a7b88638d84c34b80112399e80a4c9527a
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-09-06 02:28:52 -0500

    Cell Changes

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -20,9 +20,9 @@ const Cell = ({ q, r, i}) => {
         <Hexagon onClick={()=>handleClick(q, r, i)} key={i} q={q} r={r}>
             
             
-            {/* {<Text>
+            {<Text>
                 {i}
-            </Text>} */}
+            </Text>}
             {/* {isPlayerCell ? <Player /> : null} */}
             {randomPlace1.i === i ? <Player playerNumber={1}/> : null}
             {randomPlace2.i === i ? <Player playerNumber={2}/> : null}
