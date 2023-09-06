eb1869bfbb5ce77053aa53cd5299bf6d7136c3a1
Rockojoe2 <lee.d.joey@gmail.com>
2023-09-06 11:19:35 -0500

    Deleted node modules

--- Post-React/shootem-up-showdown/node_modules/react-hexgrid/lib/models/Hex.js
+++
@@ -1,9 +0,0 @@
-export class Hex {
-    constructor(q, r, s) {
-        this.q = q;
-        this.r = r;
-        this.s = s;
-    }
-}
-export default Hex;
-//# sourceMappingURL=Hex.js.map
--- Post-React/shootem-up-showdown/node_modules/react-hexgrid/lib/models/Point.js
+++
@@ -1,8 +0,0 @@
-export class Point {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-export default Point;
-//# sourceMappingURL=Point.js.map
