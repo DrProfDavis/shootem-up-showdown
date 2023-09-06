a06f3a067ab172d336d1f4a53a4e1ef865f257f9
Rockojoe2 <lee.d.joey@gmail.com>
2023-09-06 11:11:52 -0500

    Fixed readme, moved adjacenttiles.js out of handleclick.js, made playermovement.js that i'm still working on

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -14,36 +14,46 @@ const randomPlace2 = PlayerSpawn2();
 
 const Cell = ({ q, r, i }) => {
     const [clickedPlayer, setClickedPlayer] = useState(null);
+   
+    const [playerLocation, setPlayerLocation] = useState(() => {
+        if (randomPlace1.i === i) {
+            return 1; // Player 1 is in this location
+        } else if (randomPlace2.i === i) {
+            return 2; // Player 2 is in this location
+        } else {
+            return null; // No player is in this location initially
+        }
+    });
+
 
 
     useEffect(() => {
         if (clickedPlayer !== null) {
             console.log("Clicked Player ", clickedPlayer);
             
+            if (clickedPlayer) {
 
-            // Check if it's the player's turn (you may need to manage player turns)
-            if (clickedPlayer.playerNumber === 1) {
-                // Calculate adjacent cells
                 const adjacentCells = AdjacentTiles(q, r, i);
 
                 console.log("These are the adjacent Cells ", adjacentCells);
 
-                // Move the player to a random adjacent tile
-                movePlayerToRandomAdjacentTile(q, r, i, clickedPlayer.playerNumber, (newPosition) => {
-                    // Update the player's position in your state or game data
-                    // You need to implement a state management mechanism here
-                    console.log(`Player ${clickedPlayer.playerNumber} moved to ${newPosition.q},${newPosition.r}`);
+                movePlayerToRandomAdjacentTile(q, r, i, clickedPlayer.playerNumber, newPosition => {
+                    console.log(`Player ${clickedPlayer.playerNumber} moved to i: ${newPosition.i} q: ${newPosition.q}, r: ${newPosition.r}`);
+
+                    setPlayerLocation(clickedPlayer.playerNumber);
                 });
             }
         }
-    }, [clickedPlayer, q, r, i]);
+    }, [clickedPlayer]);
 
     return (
         <Hexagon onClick={() => { HandleClick(q, r, i, setClickedPlayer, randomPlace1, randomPlace2) }} key={i} q={q} r={r}>
             {<Text>{i} {q} {r}</Text>}
             {/* {isPlayerCell ? <Player /> : null} */}
-            {randomPlace1.i === i ? <Player playerNumber={1} /> : null}
-            {randomPlace2.i === i ? <Player playerNumber={2} /> : null}
+            {/* {randomPlace1.i === i ? <Player playerNumber={1} /> : null}
+            {randomPlace2.i === i ? <Player playerNumber={2} /> : null} */}
+            {playerLocation === 1 ? <Player playerNumber={1} /> : null}
+            {playerLocation === 2 ? <Player playerNumber={2} /> : null}
 
         </Hexagon>
     )
--- src/components/PlayerMovement.js
+++ src/components/PlayerMovement.js
@@ -5,7 +5,7 @@ import GridArray from './GridArray';
 const gridArray = GridArray;
 
 // Function to move the player to a random adjacent tile
-const movePlayerToRandomAdjacentTile = (q, r, i, playerNumber, setPlayerPosition) => {
+const movePlayerToRandomAdjacentTile = (q, r, i, playerNumber, newPosition) => {
     const adjacentTiles = AdjacentTiles(q, r, i);
 
     if (adjacentTiles.length > 0) {
@@ -16,8 +16,8 @@ const movePlayerToRandomAdjacentTile = (q, r, i, playerNumber, setPlayerPosition
         // Calculate the new coordinates from the selected adjacent tile's index
         const [newQ, newR] = gridArray[randomAdjacentTileIndex];
 
-        // Update the player's position using the setPlayerPosition function
-        setPlayerPosition({ q: newQ, r: newR, i: randomAdjacentTileIndex, playerNumber });
+        // Update the player's position using the newPosition function
+        newPosition({ q: newQ, r: newR, i: randomAdjacentTileIndex, playerNumber });
     }
 };
 
