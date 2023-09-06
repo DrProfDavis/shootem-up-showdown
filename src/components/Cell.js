037c88063b452598b6f1d6ddd911f217af508022
Rockojoe2 <lee.d.joey@gmail.com>
2023-09-05 20:28:09 -0500

    got players to console log player is clicked and shows the paths they should be able to take, can't get them moving though

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
 import { Hexagon, Text } from 'react-hexgrid'
 import {Player} from './Player' // Import the Player component
 import { HandleClick } from './HandleClick'
@@ -8,24 +8,21 @@ import {PlayerSpawn1, PlayerSpawn2} from './PlayerSpawn'
 
 const randomPlace1 = PlayerSpawn1();
 const randomPlace2 = PlayerSpawn2();
-const handleClick = HandleClick;
+// const handleClick = HandleClick;
 
 
-const Cell = ({ q, r, i}) => {
 
+    const Cell = ({ q, r, i}) => {
+    const [clickedPlayer, setClickedPlayer] = useState(null);
 
 
     return (
-        <Hexagon onClick={()=>handleClick(q, r, i)} key={i} q={q} r={r}>
-            
-            
-            {<Text>
-                {i}
-            </Text>}
+        <Hexagon onClick={() => { HandleClick(q, r, i, setClickedPlayer, randomPlace1, randomPlace2)}} key={i} q={q} r={r}>
+            {<Text>{i}</Text>}
             {/* {isPlayerCell ? <Player /> : null} */}
-            {randomPlace1.i === i ? <Player playerNumber={1}/> : null}
-            {randomPlace2.i === i ? <Player playerNumber={2}/> : null}
-        </Hexagon>
+            {randomPlace1.i === i ? <Player playerNumber={1} /> : null}
+            {randomPlace2.i === i ? <Player playerNumber={2} /> : null}
+          </Hexagon>
     )
 }
 export default Cell
--- src/components/HandleClick.js
+++ src/components/HandleClick.js
@@ -3,7 +3,8 @@ import GridArray from './GridArray'
 
 const gridArray = GridArray;
 
-const HandleClick = (q, r, i) => {
+// const HandleClick = (q, r, i, setClickedPlayer, randomPlace1, randomPlace2) => {
+    const HandleClick = (q, r, i, setClickedPlayer, randomPlace1, randomPlace2) => {
 
     const findIndexByCoordinates = (q, r) => {
         return gridArray.findIndex(coord => coord[0] === q && coord[1] === r);
@@ -14,43 +15,55 @@ const HandleClick = (q, r, i) => {
         const adjacentTiles = [];
 
         //Top
-        if(findIndexByCoordinates(q, r - 1) >= 0){
+        if (findIndexByCoordinates(q, r - 1) >= 0) {
             adjacentTiles.push(findIndexByCoordinates(q, r - 1));
         }
         //Top Right
-        if(findIndexByCoordinates(q + 1, r - 1) >= 0){
+        if (findIndexByCoordinates(q + 1, r - 1) >= 0) {
             adjacentTiles.push(findIndexByCoordinates(q + 1, r - 1));
         }
         //Bottom Right
-        if(findIndexByCoordinates(q + 1, r) >= 0){
+        if (findIndexByCoordinates(q + 1, r) >= 0) {
             adjacentTiles.push(findIndexByCoordinates(q + 1, r));
         }
-         //Bottom
-         if(findIndexByCoordinates(q, r + 1) >= 0){
+        //Bottom
+        if (findIndexByCoordinates(q, r + 1) >= 0) {
             adjacentTiles.push(findIndexByCoordinates(q, r + 1));
         }
-         //Bottom Left
-         if(findIndexByCoordinates(q - 1, r + 1) >= 0){
+        //Bottom Left
+        if (findIndexByCoordinates(q - 1, r + 1) >= 0) {
             adjacentTiles.push(findIndexByCoordinates(q - 1, r + 1));
         }
-         //Top Left
-         if(findIndexByCoordinates(q - 1, r) >= 0){
+        //Top Left
+        if (findIndexByCoordinates(q - 1, r) >= 0) {
             adjacentTiles.push(findIndexByCoordinates(q - 1, r));
         }
 
-        //These should never be negative so they'll always be pushed. JUST KIDDING. Need the if statement because if you click the bottom tiles, they get out of map and therefore the i becomes negative apparently.
-        // adjacentTiles.push(findIndexByCoordinates(q + 1, r));
-        // adjacentTiles.push(findIndexByCoordinates(q, r + 1));
-        
-        
 
-        console.log("Clicked Tile Index:", i);
-        console.log("Adjacent Tile Indexes:", adjacentTiles);
+        // console.log("Clicked Tile Index:", i);
+        // console.log("Adjacent Tile Indexes:", adjacentTiles);
+
+        const playerOnClickedTile1 = randomPlace1.i === i ? { playerNumber: 1 } : null;
+        const playerOnClickedTile2 = randomPlace2.i === i ? { playerNumber: 2 } : null;
+
+        if (playerOnClickedTile1 || playerOnClickedTile2) {
+            // You clicked on a player
+            const clickedPlayer = playerOnClickedTile1 || playerOnClickedTile2;
+            console.log("Clicked on Player:", clickedPlayer.playerNumber);
+            setClickedPlayer(clickedPlayer);
+            console.log("Your options to move are to the following spaces: ", adjacentTiles);
+
+            
+
+        } else {
+            // You did not click on a player
+            console.log("This is the i tile that doesn't have a player:", i);
+            setClickedPlayer(null);
+        }
+
+
     }
 
-    // Your click event logic goes here
-    // alert(i);
-    // console.log(i);
 }
 
-export {HandleClick};
+export { HandleClick };
