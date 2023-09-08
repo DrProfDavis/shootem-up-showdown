177a2e12e1c084a236bccbb927f94e9eb6a914d4
katyn-sh <tanhung.huynh@protonmail.ch>
2023-09-08 14:25:16 -0500

    styling for login, signup and home pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,23 @@
 import React from 'react';
-
 import { Link } from 'react-router-dom';
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button, Container, Row, Col } from 'react-bootstrap';
 
 const Home = () => {
 
   return (
-    <main>
-      <h1>HOMEPAGE</h1>
-      <button><Link to="/login">LOG IN</Link></button>
-
-      <br></br>
-      <br></br>
-
-      <button><Link to="/signup">SIGN UP</Link></button>
-      <br></br>
-      <br></br>
-      <button><Link to="/leaderboard">LEADERBOARD</Link></button>
+    <main className='home-div'>
+      <div className="d-grid gap-4 card p-5">
+        <h1 className="text-center">SHOOTEM UP SHOWDOWN</h1>
+        <Link to="/login" className="btn btn-block btn-secondary">LOG IN</Link>
+        <br></br>
+        <br></br>
+        <Link to="/signup" className="btn btn-block btn-secondary">SIGN UP</Link>
+        <br></br>
+        <br></br>
+        <Link to="/leaderboard" className="btn btn-block btn-secondary">LEADERBOARD</Link>
+      </div>
     </main>
   );
 };
