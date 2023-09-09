09dcbec6640da0020e9ae3fc09f92e900d5f19db
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-09-08 19:53:55 -0500

    edis turn

--- client/src/components/Cell.js
+++ client/src/components/Cell.js
@@ -16,28 +16,28 @@ const randomPlace2 = PlayerSpawn2();
 
 
 const Cell = ({ q, r, i }) => {
-    const [clickedPlayer, setClickedPlayer] = useState(0);
+    // const [clickedPlayer, setClickedPlayer] = useState(0);
 
-    const [playerLocations, setPlayerLocations] = useState({
-        player1: randomPlace1.i,
-        player2: randomPlace2.i,
-    });
+    // const [playerLocations, setPlayerLocations] = useState({
+    //     player1: randomPlace1.i,
+    //     player2: randomPlace2.i,
+    // });
 
     return (
         <Hexagon onClick={() => {
-            HandleClick(q, r, i, setClickedPlayer, randomPlace1, randomPlace2, playerLocations,);
+            // HandleClick(q, r, i);
 
             // console.log(playerLocations);
             
-            MovePlayer(1, q, r, i, setClickedPlayer, playerLocations, setPlayerLocations, randomPlace1, randomPlace2);
+            // MovePlayer(1, q, r, i);
             
 
             // MovePlayer(2, q, r, i, setClickedPlayer, playerLocations, setPlayerLocations, randomPlace1, randomPlace2);
 
         }} key={`${i}-${q}-${r}`} q={q} r={r}>
             {<Text>{i} {q} {r}</Text>}
-            {playerLocations.player1 === i ? <Player playerNumber={1} /> : null}
-            {playerLocations.player2 === i ? <Player playerNumber={2} /> : null}
+            {/* {playerLocations.player1 === i ? <Player playerNumber={1} /> : null}
+            {playerLocations.player2 === i ? <Player playerNumber={2} /> : null} */}
 
 
         </Hexagon>
