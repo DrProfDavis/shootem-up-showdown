c3bc71165214df645750eb3bab0b446b86285e84
May <maypham@utexas.edu>
2023-09-09 19:18:25 -0500

    added background to all pages and deleted unnecessary pages

--- client/src/pages/HowToPlay.js
+++ client/src/pages/HowToPlay.js
@@ -5,18 +5,20 @@ import { Link } from 'react-router-dom';
 const HowToPlay = () => {
 
   return (
-    <main>
-      <button><Link to="/login">LOG IN</Link></button>
+    <div className="howtoplay main-container center">
+      <main>
+        <button><Link to="/login">LOG IN</Link></button>
 
-      <br></br>
-      <br></br>
+        <br></br>
+        <br></br>
 
-      <button><Link to="/signup">SIGN UP</Link></button>
-      <br></br>
-      <br></br>
-      <button><Link to="/leaderboard">LEADERBOARD</Link></button>
-      <h1>HowToPlay</h1>
-    </main>
+        <button><Link to="/signup">SIGN UP</Link></button>
+        <br></br>
+        <br></br>
+        <button><Link to="/leaderboard">LEADERBOARD</Link></button>
+        <h1>HowToPlay</h1>
+      </main>
+    </div>
   );
 };
 
