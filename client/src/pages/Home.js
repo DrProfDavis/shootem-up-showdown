36fe8a8c5ca14e96934a02564ea27acb20e6cf7f
katyn.sh <tanhung.huynh@protonmail.ch>
2023-09-09 19:32:10 -0500

    fonts and animation base

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Container, Row, Col } from 'react-bootstrap';
+import 'animate.css';
 
 const Home = () => {
 
   return (
     <main className='home-div'>
       <div className="d-grid gap-4 card p-5">
-        <h1 className="text-center">SHOOTEM UP SHOWDOWN</h1>
+        <h1 className="text-center home-text">SHOOTEM UP SHOWDOWN</h1>
         <Link to="/login" className="btn btn-block btn-secondary">LOG IN</Link>
         <br></br>
         <br></br>
