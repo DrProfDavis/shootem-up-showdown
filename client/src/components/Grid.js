fab136adcb74f0d616e9af9b62f71815ab3d0d53
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-09-09 20:49:04 -0500

    Bullets counter added

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -7,6 +7,7 @@ import { PlayerSpawn } from './PlayerSpawn'
 import { EnemySpawn1, EnemySpawn2 } from './EnemySpawn';
 import { FriendlySpawn1, FriendlySpawn2 } from './FriendlySpawn';
 import GameOverScreen from "./GameOver"; 
+import { HandleClick } from "./HandleClick";
 
 const gridArray = GridArray;
 
@@ -36,6 +37,8 @@ const Grid = () => {
         friendly2: randomFriendlyPlace2.i,
     });
 
+    const [bullets, setBulletCount] = useState(6)
+
     const [timer, setTimer] = useState(10); // Initialize the timer stat
 
 
@@ -52,6 +55,10 @@ const Grid = () => {
     }, [score]);
 
     useEffect(() => {
+        console.log("Bullets has been updated: ", bullets);
+    }, [bullets]);
+
+    useEffect(() => {
         const interval = setInterval(() => {
             setTimer((prevTimer) => {
                 if (prevTimer > 0) {
@@ -65,9 +72,9 @@ const Grid = () => {
         return () => clearInterval(interval);
     }, []);
 
-    if (timer <= 0) {
-        return <GameOverScreen />;
-      }
+    // if (timer <= 0) {
+    //     return <GameOverScreen />;
+    //   }
 
     return (
         <div className="timerDiv">
@@ -76,14 +83,31 @@ const Grid = () => {
             </div>
 
             <div className="app">
-                <Dashboard></Dashboard>
+                <Dashboard bullets={bullets}></Dashboard>
                 <div className='gameboard'>
                     <HexGrid width={1200} height={675}>
                         <Layout spacing={1.05} size={{ x: 6, y: 6 }} origin={{ x: 0, y: 0 }}>
                             {gridArrayState.map((coord, i) => {
                                 const [q, r] = coord
                                 return (
-                                    <Cell key={`${i}-${q}-${r}`} q={q} r={r} i={i} setScore={setScore} score={score} setPlayerLocation={setPlayerLocation} playerLocation={playerLocation} setEnemyLocation={setEnemyLocation} enemyLocations={enemyLocations} setFriendlyLocation={setFriendlyLocation} friendlyLocations={friendlyLocations} setTimer={setTimer} timer={timer} />
+                                    <Cell 
+                                    key={`${i}-${q}-${r}`} 
+                                    q={q} 
+                                    r={r} 
+                                    i={i} 
+                                    setScore={setScore} 
+                                    score={score} 
+                                    setPlayerLocation={setPlayerLocation} 
+                                    playerLocation={playerLocation} 
+                                    setEnemyLocation={setEnemyLocation} 
+                                    enemyLocations={enemyLocations} 
+                                    setFriendlyLocation={setFriendlyLocation} 
+                                    friendlyLocations={friendlyLocations} 
+                                    setTimer={setTimer} 
+                                    timer={timer}
+                                    setBulletCount={setBulletCount}
+                                    bullets={bullets}
+                                    />
                                 )
                             })}
                         </Layout>
--- client/src/components/HandleClick.js
+++ client/src/components/HandleClick.js
@@ -1,29 +1,9 @@
 /*eslint-disable */
 // import { React, useState, useEffect } from 'react';
-import { AdjacentTiles } from './AdjacentTiles';
 
 
-const HandleClick = (q, r, i, setClickedPlayer, randomPlace1, randomPlace2,) => {
-
-
-    const adjacentTiles = AdjacentTiles(q, r);
-
-    const playerOnClickedTile1 = randomPlace1.i === i ? { playerNumber: 1 } : "lsdfj";
-    const playerOnClickedTile2 = randomPlace2.i === i ? { playerNumber: 2 } : "her";
-
-    if (playerOnClickedTile1 || playerOnClickedTile2) {
-        // You clicked on a player
-        const clickedPlayer = playerOnClickedTile1 || playerOnClickedTile2;
-        setClickedPlayer(clickedPlayer);
-        // console.log("Clicked player is ", clickedPlayer);
-
-    } else {
-        // You did not click on a player
-        console.log("This is the "+i+" tile that doesn't have a player:", i);
-        console.log("The adjacent tiles are ", adjacentTiles)
-        setClickedPlayer(null);
-    }
-
+const HandleClick = (bullets, setBulletCount) => {
+    setBulletCount((prevBullets) => prevBullets - 1)
 }
 
 export { HandleClick };
