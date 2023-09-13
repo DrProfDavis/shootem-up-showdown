dac0ae440db51357b417b3b1e52af16ebc2062d0
May <maypham@utexas.edu>
2023-09-13 11:13:05 -0500

    styling

--- client/src/pages/HowToPlay.js
+++ client/src/pages/HowToPlay.js
@@ -8,7 +8,7 @@ import { Card } from 'react-bootstrap';
 const HowToPlay = () => {
 
   return (
-    <div className="howtoplay main-container center content">
+    <div className="howtoplay main-container center ">
       <main>
         <Link to="/" className="btn btn-lg btn-light btn-block btn-style">🔙 HOME</Link>
 
@@ -17,8 +17,21 @@ const HowToPlay = () => {
 
         <h1>HowToPlay</h1>
         <Card className='card-txt'>
-          <p>Practice your aim here in the wild west!</p>
-          <p>Try to hit as many cowboys as you can! Each cowboy will increase your time! When your time hits 0, it's game over! It's also game over if you hit a cowgirl, so avoid that at all costs! Your score will be tallied up in the end based on the number of cowboys that you hit!</p>
+          <h3>Objective: </h3>
+
+          <p>Your goal is to accumulate the highest score possible by hitting as many cowboys as you can while avoiding cowgirls. Keep an eye on your time, as it's crucial to stay in the game.
+          </p>
+          
+          <h3>Instructions:</h3>
+          
+          <ul>
+          <li> Your game begins with a certain amount of time on the clock.</li>
+          <li>Hit as many cowboys as you can to increase your remaining time.</li>
+          <li>Every cowboy you hit adds to your time, extending your gameplay.</li>
+          <li>Be extremely cautious and avoid hitting any cowgirls.</li>
+          <li>If you run out of time, the game is over.</li>
+          <li>Your final score will be determined by the number of cowboys you successfully hit.</li>
+          <li>Good luck, and let's see how many cowboys you can round up!</li></ul>
         </Card>
       </main>
     </div>
